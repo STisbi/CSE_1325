bfaca1fbbcfd16e81cb87df1376f5a9f13e6c928
Seth <seth@seth-VirtualBox.(none)>
2017-03-25 11:53:08 -0500

    Component: torso, head, etc replaced with type

--- Homework_7/Component.h
+++ Homework_7/Component.h
@@ -1,13 +1,30 @@
 #ifndef _COMPONENT_H
 #define _COMPONENT_H
 
+// This class will be one above the individual parts
+
 class Component
 {
 	public:
+		Component(  string a_name,
+					Type a_type,
+					string a_description,
+					int a_part_num,
+					int a_weight,
+					int a_cost) :
+
+					name(a_name),
+					type(a_type),
+					description(a_description),
+					part_num(a_part_num),
+					weight(a_weight),
+					cost(a_cost) {}
+
+	private:
 		string name;
+		Type type;
+		string description;
 		int part_num;
-		enum type;
 		int weight;
 		int cost;
-		string desciption;
-}
+};
