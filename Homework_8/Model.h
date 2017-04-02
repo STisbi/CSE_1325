a617047fb32931c404523713293391f2e7c49721
Seth <seth@seth-VirtualBox.(none)>
2017-04-01 19:15:53 -0500

    Model.h creatd, compiles correctly

--- Homework_8/Model.h
+++ Homework_8/Model.h
@@ -1,4 +1,17 @@
 #ifndef _MODEL_H
 #define _MODEL_H
 
+#include <string>
 
+class Model
+{
+	private:
+		std::string mModelName;
+		int mModelNumber;
+		int mModelPrice;
+
+	public:
+		Model(std::string aModelName, int aModelNumber, int aModelPrice) : mModelName(aModelName), mModelNumber(aModelNumber), mModelPrice(aModelPrice) {}
+		int mTotal_Cost();
+};
+#endif
